35fe4e781310594be1b5cf94ee97fb6c27434457
Sahriar Mahmud <smrafsu97@gmail.com>
2024-02-18 23:46:46 +0100

    added several js codes

---
+++ scripts/utility.js
@@ -0,0 +1,28 @@
+ function hideHome(){
+    const homeSect = document.getElementById('body-container');
+    const homeVanish = homeSect.classList.add('hidden');
+    return homeVanish;
+
+}
+function showPurchase(){
+    const successPart = document.getElementById('success-part').classList.remove('hidden');
+    return successPart;
+}
+
+function play(){
+    hideHome();
+    showPurchase();
+}
+
+function continueHome(){
+    const againHome = document.getElementById('body-container').classList.remove('hidden')
+    const vanishSuccessPage = document.getElementById('success-part').classList.add('hidden');
+    const lastUpdate = againHome + vanishSuccessPage;
+    return lastUpdate
+}
+    
+
+// function colorChange(){
+//     const coloR = '#1DD100';
+//     return coloR;
+// }
