2138f522050877e413cfb00eb86766b5e4a7a7f7
Sahriar Mahmud <smrafsu97@gmail.com>
2024-02-19 08:14:11 +0100

    added the js file

--- scripts/site.js
+++ scripts/site.js
@@ -53,19 +53,25 @@ function grandTotalCost() {
     
     const totalCost = document.getElementById('total-cost').innerText;
     const convertedTotalCost = parseInt(totalCost);
-    const inputValue = document.getElementById('coupon-field').value;
+    const inputValue = document.getElementById('coupon-field').value.toLowerCase();
+    const new15code = 'NEW15'.toLowerCase();
+    const coupon20code = 'Coupon 20'.toLowerCase();
 
-    if(inputValue === 'NEW15'){
+    if(inputValue === new15code){
         const currentTotal = parseFloat(document.getElementById('grand-total').innerText);
         const newTotal = currentTotal * 0.15;
         const finalPrice = currentTotal - newTotal;
-        document.getElementById('grand-total').innerText = finalPrice.toFixed(2);;
+        document.getElementById('grand-total').innerText = finalPrice.toFixed(2);
+        document.getElementById('coupon-field').style.display = 'none';
+        document.getElementById('coupon-btn').style.display = 'none';
     }
-    else if(inputValue === 'Coupon 20'){
+    else if(inputValue === coupon20code){
         const currentTotal = parseFloat(document.getElementById('grand-total').innerText);
         const newTotal2 = currentTotal * 0.20;
         const finalPrice2 = currentTotal - newTotal2;
         document.getElementById('grand-total').innerText = finalPrice2.toFixed(2);
+        document.getElementById('coupon-field').style.display = 'none';
+        document.getElementById('coupon-btn').style.display = 'none';
       } 
       else if(inputValue === ''){
         setInnerText('grand-total', convertedTotalCost);
@@ -73,13 +79,10 @@ function grandTotalCost() {
     else {
         setInnerText('grand-total', convertedTotalCost);
         alert("Please enter a valid coupon in the input field.");
-        document.getElementById('grand-total').value = '';
-
+        document.getElementById('coupon-field').value = '';
     }
-    
-        
-    
 }
+
 
 function setInnerText(id, value) {
     document.getElementById(id).innerText = value;
@@ -87,6 +90,18 @@ function setInnerText(id, value) {
 function colorChange() {
     const coloR = '#1DD100';
     return coloR;
+}
+
+
+function submission(){
+    const nameValue = document.getElementById('name').value;
+    
+    const numberValue = document.getElementById('number').value;
+    
+    const finalButton = document.getElementById('final-btn');
+
+
+    finalButton.disabled = numberValue.length === 0;
 }
 
 
